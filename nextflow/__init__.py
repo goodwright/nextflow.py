c91f00763c1ded4466ec1cd61017867a7a155533
Sam Ireland <mail@samireland.com>
2021-09-26 15:46:34 +0100

    Execution history data

--- nextflow/__init__.py
+++ nextflow/__init__.py
@@ -8,10 +8,34 @@ class Execution:
     
 
     @property
-    def datetime(self):
+    def history_data(self):
         with open(f"{self.location}/.nextflow/history") as f:
             lines = f.readlines()
         for line in lines:
             values = line.split("\t")
-            if values[2] == self.id: return values[0]
+            if values[2] == self.id: return values
+
+    
+    @property
+    def datetime(self):
+        data = self.history_data
+        if data: return data[0]
+    
+
+    @property
+    def duration(self):
+        data = self.history_data
+        if data: return data[1]
+    
+
+    @property
+    def status(self):
+        data = self.history_data
+        if data: return data[3]
+    
+
+    @property
+    def command(self):
+        data = self.history_data
+        if data: return data[6]
         
