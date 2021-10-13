27f78ecb0e49839e07d2036fd2c22421f123d3ba
Sam Ireland <mail@samireland.com>
2021-10-13 16:45:54 +0100

    Expand tests

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -4,8 +4,32 @@ import nextflow
 
 class Tests(TestCase):
 
-    def test(self):
-        pipeline = nextflow.Pipeline(os.path.join("tests", "integration", "pipelines", "pipeline.nf"))
-        self.assertEqual(pipeline.path, os.path.join("tests", "integration", "pipelines", "pipeline.nf"))
+    def get_path(self, name):
+        return os.path.join("tests", "integration", "pipelines", name.replace("/", os.path.sep))
+
+
+    def test_basic_pipeline(self):
+        pipeline = nextflow.Pipeline(self.get_path("pipeline.nf"))
+        self.assertEqual(pipeline.path, self.get_path("pipeline.nf"))
         self.assertIsNone(pipeline.config)
         self.assertIsNone(pipeline.schema)
+    
+
+    def test_schema(self):
+        pipeline = nextflow.Pipeline(
+            self.get_path("pipeline.nf"),
+            schema=self.get_path("schema.json")
+        )
+        self.assertEqual(pipeline.path, self.get_path("pipeline.nf"))
+        self.assertIsNone(pipeline.config)
+        self.assertEqual(pipeline.schema, self.get_path("schema.json"))
+    
+
+    def test_config(self):
+        pipeline = nextflow.Pipeline(
+            self.get_path("pipeline.nf"),
+            config=self.get_path("nextflow.config")
+        )
+        self.assertEqual(pipeline.path, self.get_path("pipeline.nf"))
+        self.assertIsNone(pipeline.schema)
+        self.assertEqual(pipeline.config, self.get_path("nextflow.config"))
