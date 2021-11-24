02ef1c368841ac93a73255efe8a6c1e72de18f99
Sam Ireland <sam.ireland.uk@gmail.com>
2021-11-24 14:37:45 +0000

    Fix windows file separator issues

--- nextflow/execution.py
+++ nextflow/execution.py
@@ -17,7 +17,10 @@ class Execution:
 
     @property
     def history_data(self):
-        with open(f"{self.location}/.nextflow/history") as f:
+        """Gets the execution's data line from .nextflow/history as a list of
+        values."""
+
+        with open(os.path.join(self.location, ".nextflow", "history")) as f:
             lines = f.readlines()
         for line in lines:
             values = line.split("\t")
@@ -26,38 +29,50 @@ class Execution:
     
     @property
     def datetime(self):
+        """The datetime at which the execution started."""
+
         data = self.history_data
         if data: return data[0]
     
 
     @property
     def duration(self):
+        """How long the execution took to complete."""
+
         data = self.history_data
         if data: return data[1]
     
 
     @property
     def status(self):
+        """The Nextflow reported status of the execution."""
+
         data = self.history_data
         if data: return data[3]
     
 
     @property
     def command(self):
+        """The command used at the terminal to run the pipeline."""
+
         data = self.history_data
         if data: return data[6]
     
 
     @property
     def log(self):
+        """Gets the full text of the execution's log file."""
+
         for filename in os.listdir(self.location):
             if ".nextflow.log" in filename:
-                with open(f"{self.location}/{filename}") as f:
+                with open(os.path.join(self.location, filename)) as f:
                     text = f.read()
                     if f"[{self.id}]" in text: return text
     
 
     def get_available_fields(self):
+        """Gets all the fields that Nextflow can report for this execution."""
+
         process = subprocess.run(
             f"nextflow log {self.id} -l",
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -68,6 +83,9 @@ class Execution:
     
 
     def get_process_paths(self):
+        """Gets the work directories of all the process executions in this
+        execution."""
+
         return subprocess.run(
             f"nextflow log {self.id}",
             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -76,6 +94,9 @@ class Execution:
 
 
     def update_process_executions(self):
+        """Creates process execution objects for the execution by interrogating
+        the log and work directories."""
+        
         field_names = self.get_available_fields()
         field_names = [f"\\${f}" for f in field_names]
         self.process_executions = []
