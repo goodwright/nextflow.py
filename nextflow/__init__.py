e1092edd5f98b3f3c7d78f3cd7483f5d34e56c98
Sam Ireland <mail@samireland.com>
2022-06-01 09:57:18 +0100

    Start 0.3

--- nextflow/__init__.py
+++ nextflow/__init__.py
@@ -4,7 +4,7 @@ from .execution import Execution
 from .pipeline import Pipeline
 
 __author__ = "Sam Ireland"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 if not which("nextflow"):
     raise NextflowNotInstalledError(
