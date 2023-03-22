b7254b38aa46427d8c5d9e1dac2b090b046f7c18
Sam Ireland <mail@samireland.com>
2023-03-22 23:27:42 +0000

    Start 0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="nextflowpy",
-    version="0.5.0",
+    version="0.6.0",
     description="A Python wrapper around Nextflow.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
