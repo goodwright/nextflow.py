a199a690bae6e618a6e609ffc6376229ca87b355
Sam Ireland <mail@samireland.com>
2023-03-24 21:28:14 +0000

    New nextflow command generation

--- nextflow/run.py
+++ nextflow/run.py
@@ -1,17 +1,74 @@
+import os
+
+def run(run_location, pipeline, version, configs, params, profiles):
+    nextflow_command = make_nextflow_command(
+        run_location, pipeline, version, configs, params, profiles
+    )
 
 
-def run(pipeline, configs=None, params=None, profiles=None, version=None, run_location=".", execution_location=None, work_location=None, log_location=None):
-    pass
+def make_nextflow_command(run_path, pipeline, version, configs, params, profiles):
+    """Generates the `nextflow run` commmand.
+    
+    :param str run_path: the location to run the pipeline in.
+    :param str pipeline: the absolute path to the pipeline .nf file.
+    :param str version: the nextflow version to use.
+    :param list configs: any config files to be applied.
+    :param dict params: the parameters to pass.
+    :param list profiles: any profiles to be applied.
+    :rtype: ``str``"""
 
-    # Set locations not provided
-    if execution_location is None: execution_location = run_location
-    if work_location is None: work_location = execution_location
-    if log_location is None: log_location = execution_location
+    env = make_nextflow_command_env_string(version)
+    if env: env += " "
+    nextflow = "nextflow -Duser.country=US"
+    configs = make_nextflow_command_config_string(configs)
+    if configs: configs += " "
+    params = make_nextflow_command_params_string(params)
+    profiles = make_nextflow_command_profiles_string(profiles)
+    command = f"{env}{nextflow} {configs}run {pipeline} {params} {profiles}"
+    return f"cd {run_path}; {command}".strip()
 
-    # What is the command to run
 
-    # Run command in run_location
+def make_nextflow_command_env_string(version):
+    """Creates the environment variable setting portion of the nextflow run
+    command string.
+    
+    :param list configs: any config files to be applied.
+    :rtype: ``str``"""
 
-    # Wait for execution to complete at execution_location
+    env = {"NXF_ANSI_LOG": "false"}
+    if version: env["NXF_VER"] = version
+    return " ".join([f"{k}={v}" for k, v in env.items()])
 
-    # Create execution object using three locations
+
+def make_nextflow_command_config_string(configs):
+    """Creates the config setting portion of the nextflow run command string.
+    Absolute paths are recommended.
+    
+    :param str version: the nextflow version to use.
+    :rtype: ``str``"""
+
+    if configs is None: configs = []
+    return " ".join(f"-c \"{c}\"" for c in configs)
+
+
+def make_nextflow_command_params_string(params):
+    """Creates the parameter setting portion of the nextflow run command
+
+    :param dict params: the parameters to pass.
+    :rtype: ``str``"""
+
+    if not params: return ""
+    return " ".join([
+        f"--{param[0]}='{param[1]}'" if param[1][0] not in "'\""
+        else f"--{param[0]}={param[1]}" for param in params.items()
+    ])
+
+
+def make_nextflow_command_profiles_string(profiles):
+    """Creates the profile setting portion of the nextflow run command string.
+    
+    :param list profiles: any profiles to be applied.
+    :rtype: ``str``"""
+    
+    if not profiles: return ""
+    return ("-profile " + ",".join(profiles))
