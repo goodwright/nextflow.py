2642388b04f5dbd51338c5aeae1ade4dced56665
Sam Ireland <mail@samireland.com>
2023-07-21 11:14:12 +0100

    Start 0.6.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Goodwright Ltd"
 author = "Sam Ireland"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.1"
+release = "0.6.2"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name="nextflowpy",
-    version="0.6.1",
+    version="0.6.2",
     description="A Python wrapper around Nextflow.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
