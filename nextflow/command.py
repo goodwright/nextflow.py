36652c3d2f7768180d1390ca370601552d02fe73
Bryan Myers <bryan.myers@ozette.com>
2025-01-16 21:00:22 -0600

    debugging

--- nextflow/command.py
+++ nextflow/command.py
@@ -10,12 +10,13 @@ from nextflow.log import (
     get_process_name_from_log,
     get_process_start_from_log,
     get_process_end_from_log,
-    get_process_status_from_log
+    get_process_status_from_log,
 )
+
 
 def run(*args, **kwargs):
     """Runs a pipeline and returns the execution.
-    
+
     :param str pipeline_path: the absolute path to the pipeline .nf file.
     :param str run_path: the location to run the pipeline in.
     :param str output_path: the location to store the output in.
@@ -36,7 +37,7 @@ def run(*args, **kwargs):
 def run_and_poll(*args, **kwargs):
     """Runs a pipeline and polls it for updates. Yields the execution after each
     update.
-    
+
     :param str pipeline_path: the absolute path to the pipeline .nf file.
     :param str run_path: the location to run the pipeline in.
     :param str output_path: the location to store the output in.
@@ -57,36 +58,73 @@ def run_and_poll(*args, **kwargs):
 
 
 def _run(
-        pipeline_path, poll=False, run_path=None, output_path=None, runner=None,
-        version=None, configs=None, params=None, profiles=None, timezone=None,
-        report=None, timeline=None, dag=None, sleep=1
+    pipeline_path,
+    poll=False,
+    run_path=None,
+    output_path=None,
+    runner=None,
+    version=None,
+    configs=None,
+    params=None,
+    profiles=None,
+    timezone=None,
+    report=None,
+    timeline=None,
+    dag=None,
+    sleep=1,
 ):
-    if not run_path: run_path = os.path.abspath(".")
+    if not run_path:
+        run_path = os.path.abspath(".")
     nextflow_command = make_nextflow_command(
-        run_path, output_path, pipeline_path, version, configs, params,
-        profiles, timezone, report, timeline, dag
+        run_path,
+        output_path,
+        pipeline_path,
+        version,
+        configs,
+        params,
+        profiles,
+        timezone,
+        report,
+        timeline,
+        dag,
     )
     if runner:
         process = None
         runner(nextflow_command)
     else:
         process = subprocess.Popen(
-            nextflow_command, universal_newlines=True, shell=True        
+            nextflow_command, universal_newlines=True, shell=True
         )
     execution = None
     while True:
+        print("Sleeping")
         time.sleep(sleep)
+        print("Get Execution")
         execution = get_execution(output_path or run_path, nextflow_command)
-        if execution and poll: yield execution
+        if execution and poll:
+            yield execution
         process_finished = not process or process.poll() is not None
         if execution and execution.return_code and process_finished:
-            if not poll: yield execution
+            if not poll:
+                yield execution
             break
 
 
-def make_nextflow_command(run_path, output_path, pipeline_path, version, configs, params, profiles, timezone, report, timeline, dag):
+def make_nextflow_command(
+    run_path,
+    output_path,
+    pipeline_path,
+    version,
+    configs,
+    params,
+    profiles,
+    timezone,
+    report,
+    timeline,
+    dag,
+):
     """Generates the `nextflow run` commmand.
-    
+
     :param str run_path: the location to run the pipeline in.
     :param str output_path: the location to store the output in.
     :param str pipeline_path: the absolute path to the pipeline .nf file.
@@ -101,17 +139,23 @@ def make_nextflow_command(run_path, output_path, pipeline_path, version, configs
     :rtype: ``str``"""
 
     env = make_nextflow_command_env_string(version, timezone, output_path)
-    if env: env += " "
+    if env:
+        env += " "
     nf = "nextflow -Duser.country=US"
     log = make_nextflow_command_log_string(output_path)
-    if log: log += " "
+    if log:
+        log += " "
     configs = make_nextflow_command_config_string(configs)
-    if configs: configs += " "
+    if configs:
+        configs += " "
     params = make_nextflow_command_params_string(params)
     profiles = make_nextflow_command_profiles_string(profiles)
     reports = make_reports_string(output_path, report, timeline, dag)
-    command = f"{env}{nf} {log}{configs}run {pipeline_path} {params} {profiles} {reports}"
-    if run_path: command = f"cd {run_path}; {command}"
+    command = (
+        f"{env}{nf} {log}{configs}run {pipeline_path} {params} {profiles} {reports}"
+    )
+    if run_path:
+        command = f"cd {run_path}; {command}"
     prefix = (str(output_path) + os.path.sep) if output_path else ""
     command = command.rstrip() + f" >{prefix}"
     command += f"stdout.txt 2>{prefix}"
@@ -122,38 +166,43 @@ def make_nextflow_command(run_path, output_path, pipeline_path, version, configs
 def make_nextflow_command_env_string(version, timezone, output_path):
     """Creates the environment variable setting portion of the nextflow run
     command string.
-    
+
     :param str version: the nextflow version to use.
     :param str timezone: the timezone to use.
     :param str output_path: the location to store the output in.
     :rtype: ``str``"""
 
     env = {"NXF_ANSI_LOG": "false"}
-    if version: env["NXF_VER"] = version
-    if timezone: env["TZ"] = timezone
-    if output_path: env["NXF_WORK"] = os.path.join(output_path, "work")
+    if version:
+        env["NXF_VER"] = version
+    if timezone:
+        env["TZ"] = timezone
+    if output_path:
+        env["NXF_WORK"] = os.path.join(output_path, "work")
     return " ".join([f"{k}={v}" for k, v in env.items()])
 
 
 def make_nextflow_command_log_string(output_path):
     """Creates the log setting portion of the nextflow run command string.
-    
+
     :param str output_path: the location to store the output in.
     :rtype: ``str``"""
 
-    if not output_path: return ""
+    if not output_path:
+        return ""
     return f"-log '{os.path.join(output_path, '.nextflow.log')}'"
 
 
 def make_nextflow_command_config_string(configs):
     """Creates the config setting portion of the nextflow run command string.
     Absolute paths are recommended.
-    
+
     :param str version: the nextflow version to use.
     :rtype: ``str``"""
 
-    if configs is None: configs = []
-    return " ".join(f"-c \"{c}\"" for c in configs)
+    if configs is None:
+        configs = []
+    return " ".join(f'-c "{c}"' for c in configs)
 
 
 def make_nextflow_command_params_string(params):
@@ -162,12 +211,13 @@ def make_nextflow_command_params_string(params):
     :param dict params: the parameters to pass.
     :rtype: ``str``"""
 
-    if not params: return ""
+    if not params:
+        return ""
     param_list = []
     for key, value in params.items():
         if not value:
             param_list.append(f"--{key}=")
-        elif value[0] in "'\"": 
+        elif value[0] in "'\"":
             param_list.append(f"--{key}={value}")
         else:
             param_list.append(f"--{key}='{value}'")
@@ -176,17 +226,18 @@ def make_nextflow_command_params_string(params):
 
 def make_nextflow_command_profiles_string(profiles):
     """Creates the profile setting portion of the nextflow run command string.
-    
+
     :param list profiles: any profiles to be applied.
     :rtype: ``str``"""
 
-    if not profiles: return ""
-    return ("-profile " + ",".join(profiles))
+    if not profiles:
+        return ""
+    return "-profile " + ",".join(profiles)
 
 
 def make_reports_string(output_path, report, timeline, dag):
     """Creates the report setting portion of the nextflow run command string.
-    
+
     :param str output_path: the location to store the output in.
     :param str report: the filename to use for the execution report.
     :param str timeline: the filename to use for the timeline report.
@@ -194,9 +245,12 @@ def make_reports_string(output_path, report, timeline, dag):
     :rtype: ``str``"""
 
     params = []
-    if report: params.append(f"-with-report {report}")
-    if timeline: params.append(f"-with-timeline {timeline}")
-    if dag: params.append(f"-with-dag {dag}")
+    if report:
+        params.append(f"-with-report {report}")
+    if timeline:
+        params.append(f"-with-timeline {timeline}")
+    if dag:
+        params.append(f"-with-dag {dag}")
     if output_path:
         for i, param in enumerate(params):
             words = param.split(" ")
@@ -207,13 +261,15 @@ def make_reports_string(output_path, report, timeline, dag):
 
 def get_execution(execution_path, nextflow_command):
     """Creates an execution object from a location.
-    
+
     :param str execution_path: the location of the execution.
     :param str nextflow_command: the command used to run the pipeline.
     :rtype: ``nextflow.models.Execution``"""
 
+    print("Doing get execution stuff")
     log = get_file_text(os.path.join(execution_path, ".nextflow.log"))
-    if not log: return
+    if not log:
+        return
     identifier = m[1] if (m := re.search(r"\[([a-z]+_[a-z]+)\]", log)) else ""
     stdout = get_file_text(os.path.join(execution_path, "stdout.txt"))
     stderr = get_file_text(os.path.join(execution_path, "stderr.txt"))
@@ -225,9 +281,15 @@ def get_execution(execution_path, nextflow_command):
     command = re.sub(r">[a-zA-Z0-9\/-]+?stdout\.txt", "", command)
     command = re.sub(r"2>[a-zA-Z0-9\/-]+?stderr\.txt", "", command).strip()
     execution = Execution(
-        identifier=identifier, stdout=stdout, stderr=stderr,
-        return_code=return_code.strip(), started=started, finished=finished,
-        command=command, log=log, path=execution_path,
+        identifier=identifier,
+        stdout=stdout,
+        stderr=stderr,
+        return_code=return_code.strip(),
+        started=started,
+        finished=finished,
+        command=command,
+        log=log,
+        path=execution_path,
         process_executions=process_executions,
     )
     for process_execution in execution.process_executions:
@@ -237,34 +299,33 @@ def get_execution(execution_path, nextflow_command):
 
 def get_process_executions(log, execution_path):
     """Creates a list of process executions from a log.
-    
+
     :param str log: the log text.
     :param str execution_path: the location of the execution.
     :rtype: ``list`` of ``nextflow.models.ProcessExecution``"""
 
     process_ids = re.findall(
-        r"\[([a-f,0-9]{2}/[a-f,0-9]{6})\] Submitted process",
-        log, flags=re.MULTILINE
+        r"\[([a-f,0-9]{2}/[a-f,0-9]{6})\] Submitted process", log, flags=re.MULTILINE
     )
     process_executions = []
     process_ids_to_paths = get_process_ids_to_paths(process_ids, execution_path)
     for process_id in process_ids:
         path = process_ids_to_paths.get(process_id, "")
-        process_executions.append(get_process_execution(
-            process_id, path, log, execution_path
-        ))
+        process_executions.append(
+            get_process_execution(process_id, path, log, execution_path)
+        )
     return process_executions
 
 
 def get_process_execution(process_id, path, log, execution_path):
     """Creates a process execution from a log and its ID.
-    
+
     :param str process_id: the ID of the process.
     :param str path: the path of the process.
     :param str log: the log text.
     :param str execution_path: the location of the execution.
     :rtype: ``nextflow.models.ProcessExecution``"""
-    
+
     stdout, stderr, returncode, bash = "", "", "", ""
     if path:
         full_path = os.path.join(execution_path, "work", path)
@@ -274,9 +335,13 @@ def get_process_execution(process_id, path, log, execution_path):
         bash = get_file_text(os.path.join(full_path, ".command.sh"))
     name = get_process_name_from_log(log, process_id)
     return ProcessExecution(
-        identifier=process_id, name=name,
-        process=name[:name.find("(") - 1] if "(" in name else name,
-        path=path, stdout=stdout, stderr=stderr, return_code=returncode,
+        identifier=process_id,
+        name=name,
+        process=name[: name.find("(") - 1] if "(" in name else name,
+        path=path,
+        stdout=stdout,
+        stderr=stderr,
+        return_code=returncode,
         bash=bash,
         started=get_process_start_from_log(log, process_id),
         finished=get_process_end_from_log(log, process_id),
