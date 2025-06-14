cb9dc50bdfae12b254f3440d4c26c2a7a3272ccf
Sam Ireland <mail@samireland.com>
2025-06-14 15:06:18 +0100

    Execution submission unit tests

---
+++ tests/unit/test_submission.py
@@ -0,0 +1,24 @@
+from unittest.mock import Mock
+from unittest import TestCase
+from nextflow.models import ExecutionSubmission
+
+class SubmissionreationTests(TestCase):
+
+    def test_can_create_execution_submission(self):
+        process = Mock()
+        submission = ExecutionSubmission(
+            pipeline_path="/main.nf",
+            run_path="/executions/123",
+            output_path="/executions/123/out",
+            log_path="/executions/123/out/nextflow.log",
+            nextflow_command="nextflow run /main.nf",
+            timezone="UTC",
+            process=process
+        )
+        self.assertEqual(submission.pipeline_path, "/main.nf")
+        self.assertEqual(submission.run_path, "/executions/123")
+        self.assertEqual(submission.output_path, "/executions/123/out")
+        self.assertEqual(submission.log_path, "/executions/123/out/nextflow.log")
+        self.assertEqual(submission.nextflow_command, "nextflow run /main.nf")
+        self.assertEqual(submission.timezone, "UTC")
+        self.assertEqual(submission.process, process)
